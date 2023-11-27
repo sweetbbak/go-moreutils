ad1929b76364e2762669b2e242c3e8d8eaeac726
meilli <sweetbabyalaska@gmail.com>
2023-11-27 00:41:41 -0900

    added minimal which util

--- which/which.go
+++ which/which.go
@@ -7,16 +7,18 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 1 {
+	if len(os.Args) == 0 {
 		fmt.Println("which <binary_name>")
 		os.Exit(0)
 	}
 
-	path, err := exec.LookPath(os.Args[1])
-	if err != nil {
-		fmt.Printf("%v not found\n", os.Args[1])
-	} else {
-		fmt.Printf("%v\n", path)
+	for _, arg := range os.Args[1:] {
+		path, err := exec.LookPath(arg)
+		if err != nil {
+			fmt.Printf("%v not found\n", arg)
+		} else {
+			fmt.Printf("%v\n", path)
+		}
 	}
 
 }

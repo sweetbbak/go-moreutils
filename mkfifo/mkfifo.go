d2e40ddec35ef6e4b40393778748f0676876b303
meilli <sweetbabyalaska@gmail.com>
2023-11-29 19:44:58 -0900

    [mkfifo] added

---
+++ mkfifo/mkfifo.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"golang.org/x/sys/unix"
+)
+
+const (
+	defaultMode = 0o660 | unix.S_IFIFO // 0x1000 - 4096
+)
+
+var mode = flag.Int("mode", defaultMode, "Mode to create the fifo")
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		fmt.Println("usage: mkfifo <path>")
+		log.Fatal("please provide a path, or multiple, to create a fifo")
+	}
+
+	for _, fi := range flag.Args() {
+		if err := unix.Mkfifo(fi, uint32(*mode)); err != nil {
+			log.Fatalf("error while creating fifo: %v", err)
+		}
+	}
+}

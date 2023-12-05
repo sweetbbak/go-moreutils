4cccd0d3142c1f31a148a4be2fd8d0451a5c18cb
meilli <sweetbabyalaska@gmail.com>
2023-12-04 15:28:29 -0900

    [basename] added an infer option that guesses the extension to remove

--- basename/basename.go
+++ basename/basename.go
@@ -12,11 +12,14 @@ import (
 var (
 	suffix   = flag.String("s", "", "suffix to trim ie: /a/b.png -> b")
 	zero     = flag.Bool("z", false, "print null byte instead of new line")
-	multiple = flag.Bool("a", false, "support multiple arguments and treat each as a name")
+	multiple = flag.Bool("a", true, "support multiple arguments and treat each as a name")
+	infer    = flag.Bool("i", false, "infer the suffix/extension to trim")
 )
 
+// idk why basename just doesnt automatically spit out all the basenames by default but thats what it does sooo...
 func Basename(w io.Writer, args []string) {
 	var names []string
+
 	if *multiple {
 		for x := range args {
 			f := args[x]
@@ -24,6 +27,15 @@ func Basename(w io.Writer, args []string) {
 			if *suffix != "" {
 				f = strings.TrimSuffix(f, *suffix)
 			}
+
+			if *infer && *suffix == "" {
+				parts := strings.Split(f, ".")
+				if len(parts) > 1 {
+					suf := fmt.Sprintf(".%s", parts[len(parts)-1])
+					f = strings.TrimSuffix(f, suf)
+				}
+			}
+
 			names = append(names, f)
 		}
 	} else {
@@ -31,8 +43,17 @@ func Basename(w io.Writer, args []string) {
 		if *suffix != "" {
 			f = strings.TrimSuffix(f, *suffix)
 		}
+
+		if *infer && *suffix == "" {
+			parts := strings.Split(f, ".")
+			if len(parts) > 1 {
+				suf := fmt.Sprintf(".%s", parts[len(parts)-1])
+				f = strings.TrimSuffix(f, suf)
+			}
+		}
 		names = append(names, f)
 	}
+
 	for k := range names {
 		if *zero {
 			fmt.Fprintf(w, "%s\x00", names[k])

5338adc1d8c293bd9c702bd4e1d399d5da603006
meilli <sweetbabyalaska@gmail.com>
2023-12-13 13:28:11 -0900

    [xargs] trying to resolve relative paths

--- xargs/xargs.go
+++ xargs/xargs.go
@@ -12,12 +12,34 @@ import (
 
 const defaultMaxArgs = 5000
 
+var maxNumber = flag.Int("n", defaultMaxArgs, "max number of arguments per command")
+
 func main() {
-	var maxNumber = flag.Int("n", defaultMaxArgs, "max number of arguments per command")
 	flag.Parse()
 	if err := run(os.Stdin, os.Stdout, os.Stderr, *maxNumber, flag.Args()...); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func runit(stdin io.Reader, stdout, stderr io.Writer, maxArgs int, args ...string) error {
+	scanner := bufio.NewScanner(stdin)
+	for scanner.Scan() {
+		sp := strings.Fields(scanner.Text())
+		exe := append(args, sp...)
+
+		go func() {
+			cmd := exec.Command(exe[0], exe[1:]...)
+			cmd.Env = os.Environ()
+			cmd.Stdin = stdin
+			cmd.Stdout = stdout
+			cmd.Stderr = stderr
+
+			if err := cmd.Start(); err != nil {
+				// return err
+			}
+		}()
+	}
+	return nil
 }
 
 func run(stdin io.Reader, stdout, stderr io.Writer, maxArgs int, args ...string) error {
@@ -33,6 +55,7 @@ func run(stdin io.Reader, stdout, stderr io.Writer, maxArgs int, args ...string)
 	}
 
 	argsLen := len(args)
+	args[0] = os.ExpandEnv(args[0])
 
 	for i := 0; i < len(xArgs); i += maxArgs {
 		m := len(xArgs)
@@ -42,6 +65,7 @@ func run(stdin io.Reader, stdout, stderr io.Writer, maxArgs int, args ...string)
 		args = append(args, xArgs[i:m]...)
 
 		cmd := exec.Command(args[0], args[1:]...)
+		cmd.Env = os.Environ()
 		cmd.Stdin = stdin
 		cmd.Stdout = stdout
 		cmd.Stderr = stderr

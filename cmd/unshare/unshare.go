042dd4e4ea7d0272a9055dd4e6445653ab32733c
mizi <sweetbabyalaska@gmail.com>
2024-01-22 11:55:39 -0900

    [unshare] update, set default unshare options and allow environment manipulation

--- cmd/unshare/unshare.go
+++ cmd/unshare/unshare.go
@@ -4,18 +4,22 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"syscall"
 
 	"github.com/jessevdk/go-flags"
 )
 
 var opts struct {
-	ipc   bool `short:"i" long:"ipc" description:"Unshare the IPC namespace"`
-	mount bool `short:"m" long:"mount" description:"Unshare the mount namespace"`
-	pid   bool `short:"p" long:"pid" description:"Unshare the PID namespace"`
-	net   bool `short:"n" long:"net" description:"Unshare the net namespace"`
-	uts   bool `short:"U" long:"uts" description:"Unshare the uts namespace"`
-	user  bool `short:"u" long:"user" description:"Unshare the user namespace"`
+	ipc    bool     `short:"i" long:"ipc" description:"Unshare the IPC namespace"`
+	mount  bool     `short:"m" long:"mount" description:"Unshare the mount namespace"`
+	pid    bool     `short:"p" long:"pid" description:"Unshare the PID namespace"`
+	net    bool     `short:"n" long:"net" description:"Unshare the net namespace"`
+	uts    bool     `short:"U" long:"uts" description:"Unshare the uts namespace"`
+	user   bool     `short:"u" long:"user" description:"Unshare the user namespace"`
+	env    bool     `short:"E" long:"preserve-env" description:"preserve environment variables"`
+	all    bool     `short:"a" long:"all" description:"Unshare all options provided"`
+	SetEnv []string `short:"e" long:"env" description:"set environment variables for command ex: (--env USER=suwu)"`
 }
 
 func getShell() string {
@@ -24,6 +28,17 @@ func getShell() string {
 		sh = "/bin/bash"
 	}
 	return sh
+}
+
+func setEnv(args []string) {
+	for _, v := range args {
+		if strings.IndexByte(v, '=') > 0 {
+			kv := strings.SplitN(v, "=", 2) // split key/value pair in 2
+			if err := os.Setenv(kv[0], kv[1]); err != nil {
+				log.Println(err)
+			}
+		}
+	}
 }
 
 func unshare(args []string) error {
@@ -52,6 +67,14 @@ func unshare(args []string) error {
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 
+	if opts.env {
+		c.Env = os.Environ()
+	}
+
+	if len(opts.SetEnv) > 0 {
+		c.Env = append(c.Env, opts.SetEnv...)
+	}
+
 	if err := c.Run(); err != nil {
 		return err
 	}
@@ -63,6 +86,12 @@ func main() {
 	if err != nil {
 		os.Exit(0)
 	}
+
+	if !opts.ipc && !opts.mount && !opts.net && !opts.pid && !opts.user && !opts.uts {
+		opts.pid = true
+		opts.user = true
+	}
+
 	if err := unshare(args); err != nil {
 		log.Fatal(err)
 	}

b9db2604ec3750cb8e74fdb5f62e850006a89c50
mizi <sweetbabyalaska@gmail.com>
2024-01-23 11:58:20 -0900

    [insmod] added

--- cmd/insmod/insmod.go
+++ cmd/insmod/insmod.go
@@ -16,7 +16,13 @@ func main() {
 	}
 
 	if os.Args[1] == "--help" || os.Args[1] == "-h" {
-		println(usage)
+		println("Usage:")
+		println("    " + usage)
+		println("\nDescription:")
+		println("   insmod is a simple program to insert a module into the Linux Kernel")
+		println("   insmod takes a path to a .ko file and optional commandline parameters for the module")
+		println("   for more information, see [man insmod(8)] and see dmesg for verbose errors")
+		os.Exit(0)
 	}
 
 	filename := os.Args[1]
